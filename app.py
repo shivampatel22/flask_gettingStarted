9c2d1b9eaffcbbaf8046ec41c1898531d77f1912
root <root@DESKTOP-6A3H327.localdomain>
2022-01-22 12:53:06 +0530

    Pushing application code

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import (Flask, render_template, abort, jsonify, 
+                    request, redirect, url_for)
+from model import db, save_db
+
+app = Flask(__name__)
+
+@app.route("/welcome")
+def date():
+    return render_template(
+        "welcome.html",
+        cards=db
+    )
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template(
+        "card.html",
+        card=card,
+        index=index,
+        max_index=len(db)-1
+    )
+    except IndexError:
+        abort(404)
+
+@app.route('/api/card/<int:index>')
+def api_card_details(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
+
+@app.route('/api/card/')
+def api_card_list():
+    return jsonify(db)
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        new_card = {"agent": request.form['agent'],
+        "type": request.form['type']}
+        db.append(new_card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+
+    else:
+        return render_template("add_card.html")
+
+@app.route('/remove_card/<int:index>', methods=["GET", "POST"])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('date'))
+        else:
+            return render_template('remove_card.html', card=db[index])
+    except IndexError:
+        abort(404)
---
+++ model.py
@@ -0,0 +1,11 @@
+import json
+
+def load_db():
+    with open("app_db.json") as f:
+        return json.load(f)
+
+def save_db():
+    with open("app_db.json", 'w') as f:
+        return json.dump(db, f)
+
+db = load_db()
